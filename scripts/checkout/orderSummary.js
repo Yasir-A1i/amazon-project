cccbb4891f322276d4bf893cd4d76a867f7fd9fb
Yasir-A1i <velyfreelancer@gmail.com>
2024-10-22 10:38:37 -0700

    text for order Summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -42,6 +42,7 @@ export function renderOrderSummary() {
     cartSummaryHTML +=
       `
       <div class="cart-item-container 
+      js-cart-item-container
       js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Delivery date: ${dateString}
@@ -58,7 +59,8 @@ export function renderOrderSummary() {
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)} 
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity 
+                  js-product-quantity-${matchingProduct.id}">
                     <span>
                       Quantity: <span class="quantity-label quantity-label-visible js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -69,7 +71,7 @@ export function renderOrderSummary() {
                     </span>
                     <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                     <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id = "${matchingProduct.id}">Save</span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link js-delete-link-${matchingProduct.id} link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
@@ -154,7 +156,7 @@ export function renderOrderSummary() {
    function updateHeaderCartQuantity() {
     const cartQuantity = calculateCartQuantity();
     document.querySelector('.js-return-to-home-link')
-      .innerHTML = `${cartQuantity} items`;
+      .innerText = `${cartQuantity} items`;
   }
   document.querySelectorAll('.js-save-quantity-link').
     forEach((link) => {
